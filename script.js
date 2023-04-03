23e32b2cd0284ceb45d11e5032d6b36c121f3d29
Caine Vercoe <caine21097@waitakerecollege.school.nz>
2023-04-03 13:35:32 +1200

    stuff

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-function toggleStyles() {
-    var style1 = document.getElementById('style1');
-    var style2 = document.getElementById('style2');
-   
-    if (style1.disabled) {
-      style1.disabled = false;
-      style2.disabled = true;
-    } else {
-      style1.disabled = true;
-      style2.disabled = false;
-    }
-  }
+
 
